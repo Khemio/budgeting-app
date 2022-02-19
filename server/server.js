7f2fd215e1deaebe5cba17c1002a97845a0d32e0
Inerizal <inerizal@gmail.com>
2022-02-19 16:49:40 +0300

    heroku testing

--- server/server.js
+++ server/server.js
@@ -22,9 +22,9 @@ if(process.env.NODE_ENV === 'development') {
 }
 
 app.use('/api/v1', router);
-app.get('/', (req, res) => { res.send('Hello from Express!')});
 
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+// app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
+app.listen(PORT)
